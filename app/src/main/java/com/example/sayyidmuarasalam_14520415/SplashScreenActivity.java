6b1e2a5ef87e478c23f3014e00e34df7e5730e00
Vr <veridetta@gmail.com>
2023-07-10 11:47:34 +0700

    radio dan checkbox

--- app/src/main/java/com/example/sayyidmuarasalam_14520415/SplashScreenActivity.java
+++ app/src/main/java/com/example/sayyidmuarasalam_14520415/SplashScreenActivity.java
@@ -1,33 +1,52 @@
 package com.example.sayyidmuarasalam_14520415;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static final int SPLASH_TIMEOUT = 2000; // Waktu tunda dalam milidetik (2 detik)
+    private CheckBox persetujuanCheckbox;
+    private RadioGroup konfirmasiRadioGroup;
+    private Button bukaAplikasiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Mengatur layout yang digunakan oleh SplashScreenActivity
         setContentView(R.layout.activity_splash_screen);
 
-        // Membuat handler untuk menangani penundaan
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Membuat intent untuk pindah ke MainActivity
+        // Inisialisasi UI Components
+        persetujuanCheckbox = findViewById(R.id.checkboxPersetujuan);
+        konfirmasiRadioGroup = findViewById(R.id.radioGroupKonfirmasi);
+        bukaAplikasiButton = findViewById(R.id.bukaAplikasiButton);
+
+        // Menyembunyikan tombol buka aplikasi saat belum ada persetujuan
+        bukaAplikasiButton.setVisibility(View.GONE);
+
+        // Mengatur listener untuk checkbox persetujuan
+        persetujuanCheckbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // Menampilkan atau menyembunyikan tombol buka aplikasi berdasarkan status checkbox
+            bukaAplikasiButton.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+        });
+
+        // Mengatur listener untuk tombol buka aplikasi
+        bukaAplikasiButton.setOnClickListener(v -> {
+            // Mendapatkan konfirmasi dari radio button
+            int selectedRadioButtonId = konfirmasiRadioGroup.getCheckedRadioButtonId();
+            RadioButton selectedRadioButton = findViewById(selectedRadioButtonId);
+            boolean konfirmasiBuka = selectedRadioButton != null && selectedRadioButton.getId() == R.id.radioButtonYa;
+
+            if (konfirmasiBuka) {
+                // Jika konfirmasi membuka aplikasi, lanjutkan ke MainActivity
                 Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(intent);
-
-                // Menutup SplashScreenActivity agar tidak dapat dikembalikan dengan tombol back
                 finish();
             }
-        }, SPLASH_TIMEOUT);
+        });
     }
 }
